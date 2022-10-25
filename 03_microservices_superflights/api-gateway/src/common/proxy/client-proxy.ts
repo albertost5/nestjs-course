4e960f04dcfdff58aeeecd7adbae8281a8e2bb9d
albertost5 <sancheztorresalberto@gmail.com>
2022-10-25 12:34:14 +0200

    Add client proxyModule, clientProxy with RabbitMQ config and userController

---
+++ 03_microservices_superflights/api-gateway/src/common/proxy/client-proxy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { RabbitMQ } from "../constants";
+
+@Injectable()
+export class ClientProxySuperFlights {
+    constructor(private readonly config: ConfigService) {}
+
+    clientProxyUsers(): ClientProxy {
+        // Connect and config to RabbitMQ
+        return ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options: {
+                urls: this.config.get('AMQP_URL'),
+                queue: RabbitMQ.UserQueue
+            }
+        });
+    }
+}
---
+++ 03_microservices_superflights/api-gateway/src/user/user.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserMSG } from 'src/common/constants';
+import { IUser } from 'src/common/interfaces/user.interface';
+import { ClientProxySuperFlights } from 'src/common/proxy/client-proxy';
+import { UserDTO } from './dto/user.dto';
+
+@Controller('api/v2/user')
+export class UserController {
+    constructor(private readonly clientProxy: ClientProxySuperFlights) {}
+    // RabbitMQ users instance
+    private _clientProxyUser = this.clientProxy.clientProxyUsers();
+
+    @Post()
+    create(@Body() userDTO: UserDTO): Observable<IUser> {
+        return this._clientProxyUser.send(UserMSG.CREATE, userDTO);
+    }
+
+    @Get()
+    findAll(): Observable<IUser[]> {
+        return this._clientProxyUser.send(UserMSG.FIND_ALL, '');
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Observable<IUser>{
+        return this._clientProxyUser.send(UserMSG.FIND_ONE, id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() userDTO: UserDTO): Observable<IUser> {
+        return this._clientProxyUser.send(UserMSG.UPDATE, { id, userDTO });
+    }
+
+    @Put(':id')
+    delete(@Param('id') id: string): Observable<any> {
+        return this._clientProxyUser.send(UserMSG.DELETE, id);
+    }
+}
